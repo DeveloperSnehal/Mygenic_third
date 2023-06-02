5ae0aa319cbc98b7c61c1f70ac76e290a9d2457a
DeveloperSnehal <developer.creativemarque@gmail.com>
2023-06-02 18:40:55 +0530

    creat index page

---
+++ header.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>Medi-tech - HTML Template | Home Page 01</title>
+	<!-- Stylesheets -->
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
+	<!--Color Switcher Mockup-->
+	<link href="css/color-switcher-design.css" rel="stylesheet">
+	<!--Color Themes-->
+	<link id="theme-color-file" href="css/color-themes/default-theme.css" rel="stylesheet">
+	<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+	<link rel="icon" href="images/favicon.png" type="image/x-icon">
+	<!-- Responsive -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+	<!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
+</head>
+
+<body>
+	<div class="page-wrapper">
+		<!-- Preloader -->
+		<div class="preloader"></div>
+		<header class="main-header">
+			<!--Header Top-->
+			<div class="header-top">
+				<div class="auto-container clearfix">
+					<div class="top-left clearfix">
+						<ul class="list">
+							<li>
+								<span class="icon fas fa-envelope"></span> 2130 Fulton Street San Diego CA 94117-1080 USA
+							</li>
+							<li>
+								<span class="icon fas fa-phone"></span> <a href="tel:+555–123–2323"> 555–123–2323</a>
+							</li>
+						</ul>
+					</div>
+					<div class="top-right clearfix">
+						<ul class="social-icons">
+							<li><a href="#"><span class="fab fa-facebook-f"></span></a></li>
+							<li><a href="#"><span class="fab fa-google"></span></a></li>
+							<li><a href="#"><span class="fab fa-twitter"></span></a></li>
+							<li><a href="#"><span class="fab fa-skype"></span></a></li>
+							<li><a href="#"><span class="fab fa-linkedin"></span></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<!-- End Header Top -->
+
+			<!-- Header Upper -->
+			<div class="header-upper">
+				<div class="inner-container">
+					<div class="auto-container clearfix">
+						<!--Info-->
+						<div class="logo-outer">
+							<div class="logo"><a href="index.html"><img src="images/mygenic-logo-tm.png" alt="" title=""></a></div>
+						</div>
+						<!--Nav Box-->
+						<div class="nav-outer clearfix">
+							<!--Mobile Navigation Toggler For Mobile-->
+							<div class="mobile-nav-toggler"><span class="icon flaticon-menu"></span></div>
+							<nav class="main-menu navbar-expand-md navbar-light">
+								<div class="navbar-header">
+									<!-- Togg le Button -->
+									<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+										<span class="icon flaticon-menu"></span>
+									</button>
+								</div>
+								<div class="collapse navbar-collapse clearfix" id="navbarSupportedContent">
+									<ul class="navigation clearfix">
+										<li class="current dropdown"><a href="#">Home</a></li>
+										<li class="dropdown"><a href="#">About us</a></li>
+										<li class="dropdown has-mega-menu"><a href="#">Pages</a>
+											<div class="mega-menu">
+												<div class="mega-menu-bar row clearfix">
+													<div class="column col-md-3 col-xs-12">
+														<h3>About Us</h3>
+														<ul>
+															<li><a href="about.html">About Us</a></li>
+															<li><a href="team.html">Our Team</a></li>
+															<li><a href="faq.html">Faq</a></li>
+															<li><a href="services.html">Services</a></li>
+														</ul>
+													</div>
+													<div class="column col-md-3 col-xs-12">
+														<h3>Doctors</h3>
+														<ul>
+															<li><a href="doctors.html">Doctors</a></li>
+															<li><a href="doctors-detail.html">Doctors Detail</a></li>
+														</ul>
+													</div>
+													<div class="column col-md-3 col-xs-12">
+														<h3>Blog</h3>
+														<ul>
+															<li><a href="blog.html">Our Blog</a></li>
+															<li><a href="blog-classic.html">Blog Classic</a></li>
+															<li><a href="blog-detail.html">Blog Detail</a></li>
+														</ul>
+													</div>
+													<div class="column col-md-3 col-xs-12">
+														<h3>Shops</h3>
+														<ul>
+															<li><a href="shop.html">Shop</a></li>
+															<li><a href="shop-single.html">Shop Details</a></li>
+															<li><a href="shoping-cart.html">Cart Page</a></li>
+															<li><a href="checkout.html">Checkout Page</a></li>
+														</ul>
+													</div>
+												</div>
+											</div>
+										</li>
+										<li class="dropdown"><a href="#">Doctors</a></li>
+										<li class="dropdown"><a href="#">Department</a></li>
+										<li class="dropdown"><a href="#">Blog</a></li>
+										<li class="dropdown"><a href="#">Shop</a></li>
+										<li><a href="contact.html">Contact</a></li>
+									</ul>
+								</div>
+							</nav>
+							<!-- Main Menu End-->
+
+							<!-- Main Menu End-->
+							<div class="outer-box clearfix">
+								<!-- Search Btn -->
+								<div class="search-box-btn"><span class="icon flaticon-search"></span></div>
+								<!-- Button Box -->
+								<div class="btn-box">
+									<a href="contact.html" class="theme-btn btn-style-one"><span class="txt">Appointment</span></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--End Header Upper-->
+
+			<!--Sticky Header-->
+			<div class="sticky-header">
+				<div class="auto-container clearfix">
+					<!--Logo-->
+					<div class="logo pull-left">
+						<a href="index.html" class="img-responsive"><img src="images/logo-small.png" alt="" title=""></a>
+					</div>
+					<!--Right Col-->
+					<div class="right-col pull-right">
+						<!-- Main Menu -->
+						<nav class="main-menu navbar-expand-md">
+							<!--Mobile Navigation Toggler For Mobile-->
+							<div class="mobile-nav-toggler"><span class="icon flaticon-menu"></span></div>
+							<div class="navbar-collapse collapse clearfix" id="navbarSupportedContent1">
+								<ul class="navigation clearfix">
+									<!--Keep This Empty / Menu will come through Javascript-->
+								</ul>
+							</div>
+						</nav><!-- Main Menu End-->
+					</div>
+				</div>
+			</div>
+			<!--End Sticky Header-->
+
+			<!-- Mobile Menu  -->
+			<div class="mobile-menu">
+				<div class="menu-backdrop"></div>
+				<div class="close-btn"><span class="icon far fa-window-close"></span></div>
+				<!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header-->
+				<nav class="menu-box">
+					<div class="nav-logo"><a href="index.html"><img src="images/nav-logo.png" alt="" title=""></a></div>
+					<ul class="navigation clearfix"><!--Keep This Empty / Menu will come through Javascript--></ul>
+				</nav>
+			</div><!-- End Mobile Menu -->
+		</header>
+		<!-- End Main Header -->
