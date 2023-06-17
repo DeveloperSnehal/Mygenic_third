f34c93f7d71dd214572897d2c9ca4fb7539b64ce
DeveloperSnehal <developer.creativemarque@gmail.com>
2023-06-17 13:26:49 +0530

    changes in header page

--- header.php
+++ header.php
@@ -13,8 +13,8 @@
 	<!------<link href="css/color-switcher-design.css" rel="stylesheet">---->
 	<!--Color Themes-->
 	<link id="theme-color-file" href="css/color-themes/default-theme.css" rel="stylesheet">
-	<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
-	<link rel="icon" href="images/favicon.png" type="image/x-icon">
+	<link rel="shortcut icon" href="images/mygenic_favicon.png" type="image/x-icon">
+	<link rel="icon" href="images/mygenic_favicon.png" type="image/x-icon">
 	<!-- Responsive -->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
